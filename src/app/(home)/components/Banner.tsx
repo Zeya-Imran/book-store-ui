44849118b58e97c7b1b8a7f7cccebd1c702727f2
Zeya Imran <khanzeya63@gmail.com>
2024-04-25 04:31:09 +0530

    mobile view added in banner

--- src/app/(home)/components/Banner.tsx
+++ src/app/(home)/components/Banner.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 const Banner = () => {
     return (
         <section className="py-8">
-            <div className="flex mx-auto w-9/12">
+            <div className="flex mx-auto md:w-9/12 w-11/12">
                 <div className="w-full relative">
                     <Image  src="/paper-bg.jpg" alt="paper-bg" width={100} height={100} sizes="100vw" className="h-72 w-full rounded-lg"/>
-                    <div className="absolute top-20 left-20 text-5xl text-primary-500 kalam-bold leading-[4rem]">
+                    <div className="absolute top-20 md:left-20 left-10 text-2xl md:text-5xl text-primary-500 kalam-bold md:leading-[4rem] z-20">
                         <h1>Read, Learn and code,</h1>
                         <h2>Search your Favourite...</h2>
                     </div>
-                    <div className="absolute bottom-0 right-20">
+                    <div className="absolute bottom-0 md:right-20 right-10">
                         <Image src="/book.png" alt="book" width={0} height={0} sizes="100vw" style={{width: 'auto', height: '20rem'}} />
                     </div>
                 </div>
